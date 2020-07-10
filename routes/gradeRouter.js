049bd896e417ac9d12fbbbf7aff7081bc559485f
Cygnus-x1br <jeanmarcel83@gmail.com>
2020-07-10 15:02:54 -0300

    Ajustes diversas conforme aula interativa

--- routes/gradeRouter.js
+++ routes/gradeRouter.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import Grade from '../controllers/gradeController.js';
+import controller from '../controllers/gradeController.js';
 //import { Grade } from '../models/gradeModel.js';
 
 const app = express();
 
-app.post('/grade/', Grade.create);
-app.get('/grade/', Grade.findAll);
-app.get('/grade/id', Grade.findOne);
-app.put('/grade/id', Grade.update);
-app.delete('/grade/id', Grade.remove);
-app.delete('/grade/', Grade.removeAll);
+app.post('/grade/', controller.create);
+app.get('/grade/', controller.findAll);
+app.get('/grade/id', controller.findOne);
+app.put('/grade/id', controller.update);
+app.delete('/grade/id', controller.remove);
+app.delete('/grade/', controller.removeAll);
 
 export { app as gradeRouter };
 //Substituido controller por Grade
