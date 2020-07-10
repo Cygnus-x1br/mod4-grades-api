c324fc97b265937aea39a923cff48a544e38bfc6
Cygnus-x1br <jeanmarcel83@gmail.com>
2020-07-10 13:35:55 -0300

    Ajustes diversas conforme aula interativa

--- routes/gradeRouter.js
+++ routes/gradeRouter.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.post('/grade/', Grade.create);
 app.get('/grade/', Grade.findAll);
-app.get('/grade/:id', Grade.findOne);
-app.put('/grade/:id', Grade.update);
-app.delete('/grade/:id', Grade.remove);
+app.get('/grade/id', Grade.findOne);
+app.put('/grade/id', Grade.update);
+app.delete('/grade/id', Grade.remove);
 app.delete('/grade/', Grade.removeAll);
 
 export { app as gradeRouter };
